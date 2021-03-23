b6fb5241d64b0024456b095af64f3273dade4097
robbot <verwolff1@gmail.com>
2021-03-23 22:19:21 +0000

    more improvements

--- app2M/main.py
+++ app2M/main.py
@@ -35,7 +35,7 @@ class PdfReport:
     def generate(self, flatmate1, flatmate2, bill):
 
         flatmate1_pay = str(round(flatmate1.pays(bill,flatmate2), 2))
-        flatmate2_pay = str(round(flatmate1.pays(bill, flatmate1), 2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
 
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
@@ -63,13 +63,22 @@ class PdfReport:
 
         webbrowser.open(self.filename)
 
+amount = float(input("Hey user enter the bill amount: "))
+period = input("What is the bill period? E.g. December 2020: ")
 
-the_bill = Bill(amount=120, period="March 2021")
-john = Flatmate(name="John", days_in_house=20)
-marry = Flatmate(name="Marry", days_in_house=25)
+name1 = input("What is your name? ")
+days_in_house1 = int(input(f"How many days did {name1} stay in the house during the bill period? "))
 
-print("John pays=", john.pays(bill=the_bill, flatmate2=marry))
-print("Marry pays=", marry.pays(bill=the_bill, flatmate2=john))
+name2 = input("What is the name of other flatmate? ")
+days_in_house2 = int(input(f"How many days did {name2} stay in the house during the bill period? "))
 
-pdf_report = PdfReport(filename="Report1.pdf")
-pdf_report.generate(flatmate1=john, flatmate2=marry,bill=the_bill)
+
+the_bill = Bill(amount, period)
+flatmate1 = Flatmate(name1, days_in_house1)
+flatmate2 = Flatmate(name2, days_in_house2)
+
+print(f"{flatmate1.name} pays=", flatmate1.pays(the_bill, flatmate2))
+print(f"{flatmate2.name} pays=", flatmate2.pays(the_bill, flatmate1))
+
+pdf_report = PdfReport(filename=f"{the_bill.period}.pdf")
+pdf_report.generate(flatmate1, flatmate2, the_bill)
