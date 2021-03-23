afcf2a8f96e768554d8b5a84ef6beb815c6a9154
robbot <verwolff1@gmail.com>
2021-03-23 18:37:49 +0000

    remove border, change size font and add icon to pdf report

--- app2M/main.py
+++ app2M/main.py
@@ -32,21 +32,30 @@ class PdfReport:
 
     def generate(self, flatmate1, flatmate2, bill):
 
+        flatmate1_pay = str(round(flatmate1.pays(bill,flatmate2), 2))
+        flatmate2_pay = str(round(flatmate1.pays(bill, flatmate1), 2))
+
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
 
+        #add icon house.png
+        pdf.image("house.png", w=30, h=30)
+
         # Insert title
         pdf.set_font(family='Times', size=24, style='B')
-        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align='C', ln=1)
-        #Insert title
-        pdf.cell(w=100, h=40, txt="Period:", border=1)
-        pdf.cell(w=150, h=40, txt=bill.period, border=1)
-        #Insert Period label and value
-        pdf.cell(w=100, h=40, txt="Period:", border=1)
-        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align='C', ln=1)
+        #Insert content
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=9, ln=1)
+
         #Insert name and due amount of the first flatmate
-        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
-        pdf.cell(w=150, h=40, txt=str(flatmate1.pays(bill,flatmate2)), border=1)
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=40, txt=flatmate1_pay, border=0, ln=1)
+        #Insert name and due amount of the second flatmate
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=40, txt=flatmate2_pay, border =0)
 
         pdf.output(self.filename)
 
