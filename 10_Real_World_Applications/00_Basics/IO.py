5e2907f7d95c6c5b152a123cc5877daf88b65256
Rob_R <1743138+Robbot@users.noreply.github.com>
2021-04-14 23:06:58 +0100

    moving folders

---
+++ 10_Real_World_Applications/00_Basics/IO.py
@@ -0,0 +1,18 @@
+myfile = open("fruits.txt")
+content = myfile.read()
+print(content)
+myfile.close()
+print(content)
+
+with open("fruits.txt") as myfile:
+    content = myfile.read()
+
+print(content)
+
+def occurences(character,filepath):
+    with open(filepath,"r") as myfile:
+        content= myfile.read()
+        myfile.close()
+    return content.count(character)
+
+print(occurences("a", "fruits.txt"))
