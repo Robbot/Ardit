c1f239bc38517ffbbb71cfe03b1eebd69f238159
Rob_R <1743138+Robbot@users.noreply.github.com>
2021-05-20 21:11:00 +0100

    the first kv app finished

--- Complete_Python/04_Webecam Photo Sharer/main.py
+++ Complete_Python/04_Webecam Photo Sharer/main.py
@@ -1,13 +1,26 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import wikipedia
+import requests
 
 
 Builder.load_file('frontend.kv')
 
 class FirstScreen(Screen):
     def search_image(self):
-        self.manager.current_screen.ids.img.source = 'files/sky.jpg'
+        #get user query from TextInput in kv file
+        query = self.manager.current_screen.ids.user_query.text
+        #Get wikipedia page and taake url of the first image from the list
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+        #Download that image
+        req = requests.get(image_link)
+        filepath = 'files/image.jpg'
+        with open(filepath, 'wb') as file:
+            file.write(req.content)
+        #Set the image in the Image widget
+        self.manager.current_screen.ids.img.source = filepath
 
 
 class RootWidget(ScreenManager):
