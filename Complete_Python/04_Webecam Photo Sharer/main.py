c650ed5337531c5aaa5dafaff8919db56da76afd
Rob_R <1743138+Robbot@users.noreply.github.com>
2021-05-20 21:18:44 +0100

    oop

--- Complete_Python/04_Webecam Photo Sharer/main.py
+++ Complete_Python/04_Webecam Photo Sharer/main.py
@@ -8,19 +8,25 @@ import requests
 Builder.load_file('frontend.kv')
 
 class FirstScreen(Screen):
-    def search_image(self):
+    def get_image_link(self):
         #get user query from TextInput in kv file
         query = self.manager.current_screen.ids.user_query.text
         #Get wikipedia page and taake url of the first image from the list
         page = wikipedia.page(query)
         image_link = page.images[0]
+        return image_link
+
+    def download_image(self):
         #Download that image
-        req = requests.get(image_link)
+        req = requests.get(self.get_image_link())
         filepath = 'files/image.jpg'
         with open(filepath, 'wb') as file:
             file.write(req.content)
+        return filepath
+
+    def set_image(self):
         #Set the image in the Image widget
-        self.manager.current_screen.ids.img.source = filepath
+        self.manager.current_screen.ids.img.source = self.download_image()
 
 
 class RootWidget(ScreenManager):
